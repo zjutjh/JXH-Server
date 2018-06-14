b0aea3870af2de134ab2ba7d02d5b5f3ef8ce085
zzhutianyu <704735206@qq.com>
2018-06-14 18:31:31 +0800

    fix JwtAuth middleware

--- app/Http/Middleware/JwtAuth.php
+++ app/Http/Middleware/JwtAuth.php
@@ -32,23 +32,15 @@ class JwtAuth extends BaseMiddleware
                 // 使用一次性登录以保证此次请求的成功
                 Auth::guard('web')->onceUsingId($this->auth->manager()->getPayloadFactory()->buildClaimsCollection()->toPlainArray()['sub']);
             } catch (JWTException $e) {
-                return $this->apiReponse(-402, '缺少登录凭证', null);
+                return RJM( null,-402, '缺少登录凭证');
             }
         } catch (TokenInvalidException $e) {
-            return $this->apiReponse(-403, '登录凭证不合法', null);
+            return RJM(null,-403, '登录凭证不合法');
         } catch (JWTException $e) {
-            return $this->apiReponse(-402, '缺少登录凭证', null);
+            return RJM(null, -402, '缺少登录凭证');
         }
         return $this->setAuthenticationHeader($next($request), $token);
     }
 
-    public function apiReponse($code,$error,$data)
-    {
-        $json = [
-            'code' => $code,
-            'error' => $error,
-            'data' => $data
-        ];
-        return response()->json($json);
-    }
+
 }
