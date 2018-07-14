ba333602babc3e93bdca6e9b1b59270e0f761907
诸天域 <704735206@qq.com>
2018-07-14 12:34:36 +0800

    fix admin page

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,12 +6,12 @@
     <meta name=viewport content="width=device-width,initial-scale=1">
     <title>精小弘 Admin</title>
     <link rel="shortcut icon" href=/logo.png>
-    <link href=/assets/css/app.93ee14e862a32fcfe282f67313f057cb.css rel=stylesheet>
+    <link href=/assets/css/app.71090f7494e8c062c835a92ef33647a2.css rel=stylesheet>
 </head>
 <body>
 <div id=app></div>
-<script type=text/javascript src=/assets/js/manifest.d137266f17c1ac58416d.js></script>
-<script type=text/javascript src=/assets/js/vendor.9adf47ca610adf84f2ef.js></script>
-<script type=text/javascript src=/assets/js/app.4436ba17e811d932367b.js></script>
+<script type=text/javascript src=/assets/js/manifest.3db713bd4a6bcd7ac652.js></script>
+<script type=text/javascript src=/assets/js/vendor.d0c10085f15c9e2b879f.js></script>
+<script type=text/javascript src=/assets/js/app.1a0d1a2a00d3adbb66f0.js></script>
 </body>
 </html>
