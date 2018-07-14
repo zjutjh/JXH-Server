d4a7626b9351998b6b66fd468a61278e91e9264c
诸天域 <704735206@qq.com>
2018-07-14 12:37:27 +0800

    fix admin login

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -11,7 +11,7 @@ class AdminController extends Controller
 {
     public function login(Request $request) {
         $username = $request->get('username');
-        $passwd = $request->get('passwd');
+        $passwd = $request->get('password');
 
         $uCenter = new UserCenterService();
         if (!$error = $uCenter->checkJhPassport($username, $passwd)) {
