e44c1d6cabc4e554860b5b587969689634f49161
zzhutianyu <704735206@qq.com>
2018-08-03 19:27:23 +0800

    update admin

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -10,9 +10,8 @@
 </head>
 <body>
 <div id=app></div>
-<script type=text/javascript src=/assets/js/manifest.b2a787f1f33379231624.js></script>
+<script type=text/javascript src=/assets/js/manifest.d540b620dc7546692f31.js></script>
 <script type=text/javascript src=/assets/js/vendor.b336ccda10b0cfd4dbbb.js></script>
-<script type=text/javascript src=/assets/js/app.6643621159dae26bbb3f.js></script>
-</body>
+<script type=text/javascript src=/assets/js/app.c4e25845fcc9d9c2bade.js></script>
 </body>
 </html>
