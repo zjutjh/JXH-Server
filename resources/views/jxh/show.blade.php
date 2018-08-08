1652a119368a18d7b7e2395f2d3a05254d6f28e7
zzhutianyu <704735206@qq.com>
2018-08-08 22:51:37 +0800

    update show page

--- resources/views/jxh/show.blade.php
+++ resources/views/jxh/show.blade.php
@@ -41,25 +41,29 @@
         </div>
     </div>
     <script>
-        wx.onMenuShareTimeline({
-            title: '{{ $message->title }}', // 分享标题
-            link: document.location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-            imgUrl: '{{ $message->cover_image }}', // 分享图标
-            success: function () {
-                // 用户点击了分享后执行的回调函数
-            }
+        wx.ready(function(){
+            // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+            wx.onMenuShareTimeline({
+                title: '{{ $message->title }}', // 分享标题
+                link: document.location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                imgUrl: '{{ $message->cover_image }}', // 分享图标
+                success: function () {
+                    // 用户点击了分享后执行的回调函数
+                }
+            });
+
+            wx.onMenuShareAppMessage({
+                title: '{{ $message->title }}', // 分享标题
+                desc: '{{ trim_words($message->content, 100) }}', // 分享描述
+                link: document.location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                imgUrl: '{{ $message->cover_image }}', // 分享图标
+                type: '', // 分享类型,music、video或link，不填默认为link
+                dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+                success: function () {
+// 用户点击了分享后执行的回调函数
+                }
+            });
         });
 
-        wx.onMenuShareAppMessage({
-            title: '{{ $message->title }}', // 分享标题
-            desc: '', // 分享描述
-            link: document.location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-            imgUrl: '{{ $message->cover_image }}', // 分享图标
-            type: '', // 分享类型,music、video或link，不填默认为link
-            dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-            success: function () {
-// 用户点击了分享后执行的回调函数
-            }
-        });
     </script>
 @endsection
